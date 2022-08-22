1b7307467462406340cb4e952f65e2d8263462cc
Linus Johansson <linus@issuefab.com>
2022-08-22 11:50:28 +0200

    card creatable validate

--- Card/Creatable/index.spec.ts
+++ Card/Creatable/index.spec.ts
@@ -8,4 +8,10 @@ describe("Card.Creatable", () => {
 	it("is", () => {
 		expect(model.Card.Creatable.is(creatable)).toEqual(true)
 	})
+	it("validate", () => {
+		expect(model.Card.Creatable.validate(creatable)).toEqual(true)
+		expect(model.Card.Creatable.validate(creatable, 99)).toEqual(false)
+		expect(model.Card.Creatable.validate(creatable, undefined, "SEK")).toEqual(false)
+		expect(model.Card.Creatable.validate(creatable, undefined, undefined)).toEqual(true)
+	})
 })
--- Card/Creatable/index.ts
+++ Card/Creatable/index.ts
@@ -10,4 +10,7 @@ export namespace Creatable {
 	export function is(value: Creatable | any): value is Creatable & Record<string, any> {
 		return typeof value == "object" && value.type == "card" && Amount.is(value.limit)
 	}
+	export function validate(value: Creatable, limit?: number, currency?: isoly.Currency) {
+		return Amount.validate(value.limit, limit, currency)
+	}
 }
