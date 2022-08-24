aac17d3825346de13081e52e9d5e99a9d4bb9907
Linus Johansson <linus@issuefab.com>
2022-08-24 11:54:09 +0200

    amount validation fix

--- Amount/index.ts
+++ Amount/index.ts
@@ -6,6 +6,6 @@ export namespace Amount {
 		return Array.isArray(value) && value.length == 2 && typeof value[0] == "number" && isoly.Currency.is(value[1])
 	}
 	export function validate(value: Amount, limit?: Amount) {
-		return value[0] > 0 && (limit == undefined || value[0] <= limit[0]) && (limit == undefined || value[1] == limit[1])
+		return value[0] > 0 && (limit == undefined || (value[0] <= limit[0] && value[1] == limit[1]))
 	}
 }
