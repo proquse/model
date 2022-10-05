08e642f769f4c7ff70a21d2b6d34e6df03c7d5bc
Elias Eriksson <eliaseriksson95@gmail.com>
2022-10-05 10:18:33 +0200

    Purchase list improvments (#41)
    * improvements
    
    * added find and list methods to interfaces
    
    * costCenter and from as parameter

--- Transaction/index.ts
+++ Transaction/index.ts
@@ -1,5 +1,6 @@
 import * as isoly from "isoly"
 import { Amount } from "../Amount"
+import { Delegation } from "../Delegation"
 import { Purchase } from "../Purchase"
 import { Link as TransactionLink } from "./Link"
 
@@ -27,6 +28,44 @@ export namespace Transaction {
 			(value.date.payment == undefined || isoly.DateTime.is(value.date.payment)) &&
 			(value.receipt == undefined || typeof value.receipt == "string")
 		)
+	}
+
+	function findInner<T, S>(elements: T[], finder: (element: T) => S | undefined): S | undefined {
+		let result: S | undefined
+		elements.find(single => (result = finder(single)))
+		return result
+	}
+	export function find(
+		roots: Delegation[],
+		id: string
+	): { root: Delegation; purchase: Purchase; found: Transaction } | undefined {
+		return findInner(roots, root => {
+			let result = findInner(root.purchases, purchase =>
+				findInner(purchase.transactions, transaction =>
+					transaction.id != id ? undefined : { root: root, purchase: purchase, found: transaction }
+				)
+			)
+			return result ?? ((result = find(root.delegations, id)) && { ...result, root: root })
+		})
+	}
+	export function list(
+		roots: Iterable<Delegation>,
+		filter?: (transaction: Transaction, purchase: Purchase, delegation: Delegation) => any
+	): Transaction[] {
+		function* list(
+			roots: Iterable<Delegation>,
+			filter?: (transaction: Transaction, purchase: Purchase, delegation: Delegation) => any
+		): Generator<Transaction> {
+			for (const root of roots) {
+				for (const purchase of root.purchases) {
+					for (const transaction of purchase.transactions) {
+						;(!filter || filter(transaction, purchase, root)) && (yield transaction)
+					}
+				}
+				yield* list(root.delegations, filter)
+			}
+		}
+		return Array.from(list(roots, filter))
 	}
 	export function validate(transaction: Transaction): boolean {
 		return (
