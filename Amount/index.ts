3f7e2b53b8bc75dd2716c307effff35bd2ba1d35
Linus Johansson <linus@issuefab.com>
2022-10-14 09:56:09 +0200

    NaN check

--- Amount/index.ts
+++ Amount/index.ts
@@ -3,7 +3,13 @@ import * as isoly from "isoly"
 export type Amount = [number, isoly.Currency]
 export namespace Amount {
 	export function is(value: Amount | any): value is Amount {
-		return Array.isArray(value) && value.length == 2 && typeof value[0] == "number" && isoly.Currency.is(value[1])
+		return (
+			Array.isArray(value) &&
+			value.length == 2 &&
+			typeof value[0] == "number" &&
+			!isNaN(value[0]) &&
+			isoly.Currency.is(value[1])
+		)
 	}
 	export function validate(amount: Amount, limit?: Amount): boolean {
 		return amount[0] > 0 && (limit == undefined || (amount[0] <= limit[0] && amount[1] == limit[1]))
