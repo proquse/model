673057d518cc8269cf2a7e03926c51364111e278
Linus Johansson <linus@issuefab.com>
2022-10-19 17:05:17 +0200

    userwidgets model imported. Roles created and initial tests created.

---
+++ Roles/index.spec.ts
@@ -0,0 +1,51 @@
+import * as userModel from "@userwidgets/model"
+import * as model from "../index"
+
+describe("Financial Controller", () => {
+	const key: userModel.User.Key = {
+		issuer: "",
+		audience: "",
+		issued: "",
+		expires: "",
+		token: "",
+		name: { first: "", last: "" },
+		email: "",
+		permissions: {
+			organizationId: {
+				organization: { read: true, write: true },
+				user: { read: true, write: true },
+				delegation: { read: true, write: true },
+				banking: { read: true, write: true },
+			},
+		},
+	}
+	it("satisfies", () => {
+		expect(model.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(true)
+	})
+})
+
+describe("user", () => {
+	const key: userModel.User.Key = {
+		issuer: "",
+		audience: "",
+		issued: "",
+		expires: "",
+		token: "",
+		name: { first: "", last: "" },
+		email: "",
+		permissions: {
+			organizationId: {
+				organization: { read: false, write: false },
+				user: { read: false, write: true },
+				delegation: { read: false, write: false },
+				banking: { read: false, write: false },
+			},
+		},
+	}
+	it("satisfies", () => {
+		expect(model.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(false)
+	})
+	it("satisfies", () => {
+		expect(model.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
+	})
+})
