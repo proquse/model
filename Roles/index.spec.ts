8225bc947c387cac0b64077d64401b2b109039f9
Linus Johansson <linus@issuefab.com>
2022-10-19 17:14:21 +0200

    tests for roles

--- Roles/index.spec.ts
+++ Roles/index.spec.ts
@@ -22,6 +22,38 @@ describe("Financial Controller", () => {
 	it("satisfies", () => {
 		expect(model.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(true)
 	})
+	it("satisfies", () => {
+		expect(model.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
+	})
+})
+
+describe("Financial Controller with App permissions", () => {
+	const key: userModel.User.Key = {
+		issuer: "",
+		audience: "",
+		issued: "",
+		expires: "",
+		token: "",
+		name: { first: "", last: "" },
+		email: "",
+		permissions: {
+			["*"]: {
+				organization: { read: true },
+			},
+			organizationId: {
+				organization: { write: true },
+				user: { read: true, write: true },
+				delegation: { read: true, write: true },
+				banking: { read: true, write: true },
+			},
+		},
+	}
+	it("satisfies", () => {
+		expect(model.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(true)
+	})
+	it("satisfies", () => {
+		expect(model.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
+	})
 })
 
 describe("user", () => {
