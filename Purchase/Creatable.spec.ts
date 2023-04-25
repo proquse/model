45fe9ed4b66ae053e336d61b1fe3f3446c3ec308
Elias Eriksson <eliaseriksson95@gmail.com>
2023-04-25 11:14:09 +0200

    added test

--- Purchase/Creatable.spec.ts
+++ Purchase/Creatable.spec.ts
@@ -20,6 +20,13 @@ describe("Purchase.Creatable", () => {
 		expect(
 			model.Purchase.Creatable.validate({ purpose: "", payment: { type: "card", limit: [10, "EUR"] }, buyer: "sasd" })
 		).toEqual(true)
+		expect(
+			model.Purchase.Creatable.validate({
+				purpose: "",
+				payment: { type: "pre-paid", limit: [10, "EUR"] },
+				buyer: "sasd",
+			})
+		).toEqual(true)
 		expect(model.Purchase.Creatable.validate(creatable, [20, "EUR"])).toEqual(true)
 		expect(model.Purchase.Creatable.validate(creatable, [5, "EUR"])).toEqual(false)
 	})
