cca95798574550fc17f3f38cb7c398c231791a09
Elias Eriksson <elias@issuefab.com>
2023-05-09 14:28:36 +0200

    receipt uses file instead of uint8array (#85)

--- Receipt/Creatable.ts
+++ Receipt/Creatable.ts
@@ -2,7 +2,7 @@ import * as isoly from "isoly"
 import { Total } from "./Total"
 export interface Creatable {
 	total: Total[]
-	file: Uint8Array
+	file: File
 }
 export namespace Creatable {
 	export function is(value: Creatable | any): value is Creatable {
@@ -11,33 +11,14 @@ export namespace Creatable {
 			value &&
 			Array.isArray(value.total) &&
 			value.total.every(Total.is) &&
-			value.file instanceof Uint8Array
+			value.file instanceof File
 		)
 	}
 	export function validate(receipt: Creatable, currency: isoly.Currency): boolean {
 		return !!(
 			receipt.total.length &&
 			receipt.total.every(total => Total.validate(total, currency)) &&
-			receipt.file instanceof Uint8Array
+			receipt.file instanceof File
 		)
-	}
-	export function formData(receipt: Creatable & { file: ArrayBuffer | Uint8Array | Blob | File }): FormData {
-		const form = new FormData()
-		form.append("data", JSON.stringify((({ file, ...data }) => data)(receipt)))
-		form.append("file", receipt.file instanceof Blob ? receipt.file : new Blob([receipt.file]))
-		return form
-	}
-	export async function parse(form: { data: string; file: Blob } | any): Promise<Creatable | undefined> {
-		let result: Creatable | undefined
-		if (typeof form != "object" || !form || typeof form.data != "string" || typeof form.file != "object" || !form.file)
-			result = undefined
-		else {
-			const parsed = JSON.parse(form.data)
-			result =
-				typeof parsed != "object" || !parsed || !(form.file instanceof Blob)
-					? undefined
-					: Object.assign(parsed, { file: new Uint8Array(await form.file.arrayBuffer()) })
-		}
-		return !is(result) ? undefined : result
 	}
 }
