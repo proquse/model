53fbaf3d817149107018f78210ac5a4dc15968f9
toli1987 <toli1987@protonmail.com>
2023-06-09 12:58:07 +0200

    add test

---
+++ Report/Expense/index.spec.ts
@@ -0,0 +1,14 @@
+import { FormData } from "formdata-polyfill/esm.min.js"
+import { Blob, File } from "web-file-polyfill"
+import { issuefab } from "../../index"
+globalThis.Blob = Blob
+globalThis.FormData = FormData
+globalThis.File = File
+
+describe("ReportExpense", () => {
+	const file = { file: new File([new Uint8Array([97])], "file") }
+
+	it("is", () => {
+		expect(issuefab.Report.Expense.type.is(file)).toEqual(true)
+	})
+})
