cdab99e7b11da901bc84d12d6902dda99f110b3c
Tobias Liljeblad <tobias@issuefab.com>
2023-06-12 10:37:07 +0200

    small changes

--- Report/Creatable.spec.ts
+++ Report/Creatable.spec.ts
@@ -2,7 +2,7 @@ import { issuefab } from "../index"
 
 describe("Expense Creatable", () => {
 	const expenseCreatable: issuefab.Report.Expense.Creatable = {
-		userEmails: ["Test@test.com"],
+		emails: ["Test@test.com"],
 		costCenterIds: ["asd2213"],
 	}
 	const receiptCreatable: issuefab.Report.Receipt.Creatable = {
@@ -15,6 +15,6 @@ describe("Expense Creatable", () => {
 		expect(issuefab.Report.Creatable.is((({ costCenterIds, ...creatable }) => creatable)(expenseCreatable))).toEqual(
 			false
 		)
-		expect(issuefab.Report.Creatable.is((({ userEmails, ...creatable }) => creatable)(expenseCreatable))).toEqual(true)
+		expect(issuefab.Report.Creatable.is((({ emails, ...creatable }) => creatable)(expenseCreatable))).toEqual(true)
 	})
 })
