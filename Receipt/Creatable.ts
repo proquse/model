2bd1f6bfa81d030a4d48f488af42a386be9c4976
Tobias Liljeblad <tobias@issuefab.com>
2023-06-13 15:48:38 +0200

    Add file type check

--- Receipt/Creatable.ts
+++ Receipt/Creatable.ts
@@ -1,24 +1,26 @@
 import { isoly } from "isoly"
+import { isly } from "isly"
 import { Total } from "./Total"
 export interface Creatable {
 	total: Total[]
 	file: File
 }
 export namespace Creatable {
-	export function is(value: Creatable | any): value is Creatable {
-		return (
-			typeof value == "object" &&
-			value &&
-			Array.isArray(value.total) &&
-			value.total.every(Total.is) &&
-			value.file instanceof File
-		)
-	}
-	export function validate(receipt: Creatable, currency: isoly.Currency): boolean {
-		return !!(
-			receipt.total.length &&
-			receipt.total.every(total => Total.validate(total, currency)) &&
-			receipt.file instanceof File
-		)
-	}
+	export const type = isly.object<Creatable>({
+		total: isly.array(Total.type),
+		file: isly.fromIs<File & { type: "image/jpeg" | "application/pdf" }>(
+			"File",
+			value => value instanceof File && !(value.type == "image/jpeg" || value.type == "application/pdf")
+		),
+	})
+
+	export const is = type.is
+	export const flaw = type.flaw
+}
+export function validate(receipt: Creatable, currency: isoly.Currency): boolean {
+	return !!(
+		receipt.total.length &&
+		receipt.total.every(total => Total.validate(total, currency)) &&
+		receipt.file instanceof File
+	)
 }
