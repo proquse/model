2a197c628bf4919fa1242f57c7691fe47c32221c
toli1987 <toli1987@protonmail.com>
2023-06-14 13:10:48 +0200

    ReceiptPreviewData done

--- Report/Receipt/PreviewData.ts
+++ Report/Receipt/PreviewData.ts
@@ -1,4 +1,5 @@
 import { isoly } from "isoly"
+import { isly } from "isly"
 import { Purchase } from "../../Purchase"
 import { Receipt } from "../../Receipt"
 
@@ -11,4 +12,24 @@ export interface PreviewData {
 	dateRange: isoly.DateRange
 }
 
-export namespace PreviewData {}
+export namespace PreviewData {
+	export const type = isly.object<PreviewData>({
+		receiptsData: isly.array(
+			isly.object({
+				costCenter: isly.string(),
+				receipts: isly.array<PreviewData["receiptsData"][number]["receipts"]>(
+					isly.object({
+						details: Receipt.type,
+						purchase: Purchase.type,
+						file: isly.fromIs<File>("File", value => value instanceof File),
+					})
+				),
+			})
+		),
+		organization: isly.string(),
+		dateRange: isly.fromIs("DateRange", isoly.DateRange.is),
+	})
+
+	export const is = type.is
+	export const flaw = type.flaw
+}
