bc862e0c59a625958c0ada296a13db4a7a5c3da7
Elias Eriksson <eliaseriksson95@gmail.com>
2023-06-27 13:15:26 +0200

    changed amount interface and implemented allocated function

--- Amount/index.spec.ts
+++ Amount/index.spec.ts
@@ -1,17 +1,95 @@
-import { issuefab } from "../index"
+// import { issuefab } from "../index"
+import { Amount } from "./index"
 
 describe("Amount", () => {
+	const year: Amount = {
+		cadence: "year",
+		value: 10,
+		currency: "EUR",
+		created: "2023-03-04",
+	}
+	const month: Amount = {
+		cadence: "month",
+		value: 10,
+		currency: "EUR",
+		created: "2023-03-04",
+	}
+	const week: Amount = {
+		cadence: "week",
+		value: 10,
+		currency: "EUR",
+		created: "2023-03-04",
+	}
+	const single: Amount = {
+		cadence: "single",
+		value: 10,
+		currency: "EUR",
+		created: "2023-03-04",
+	}
 	it("is", () => {
-		expect(issuefab.Amount.is([1, "EUR"])).toEqual(true)
+		expect(Amount.is(year)).toEqual(true)
+		expect(Amount.is(month)).toEqual(true)
+		expect(Amount.is(week)).toEqual(true)
+		expect(Amount.is(single)).toEqual(true)
+		expect(Amount.is([1, "EUR"])).toEqual(false)
 	})
 	it("validate", () => {
-		expect(issuefab.Amount.validate([0, "EUR"])).toEqual(false)
-		expect(issuefab.Amount.validate([1, "EUR"])).toEqual(true)
-		expect(issuefab.Amount.validate([1, "EUR"], [10, "EUR"])).toEqual(true)
-		expect(issuefab.Amount.validate([1, "EUR"], [10, "SEK"])).toEqual(false)
-		expect(issuefab.Amount.validate([11, "EUR"], [10, "EUR"])).toEqual(false)
-		expect(issuefab.Amount.validate([11, "EUR"], [10, "SEK"])).toEqual(false)
-		expect(issuefab.Amount.validate([-1, "EUR"])).toEqual(false)
-		expect(issuefab.Amount.validate([1, "EUR"], undefined)).toEqual(true)
+		// expect(issuefab.Amount.validate([0, "EUR"])).toEqual(false)
+		// expect(issuefab.Amount.validate([1, "EUR"])).toEqual(true)
+		// expect(issuefab.Amount.validate([1, "EUR"], [10, "EUR"])).toEqual(true)
+		// expect(issuefab.Amount.validate([1, "EUR"], [10, "SEK"])).toEqual(false)
+		// expect(issuefab.Amount.validate([11, "EUR"], [10, "EUR"])).toEqual(false)
+		// expect(issuefab.Amount.validate([11, "EUR"], [10, "SEK"])).toEqual(false)
+		// expect(issuefab.Amount.validate([-1, "EUR"])).toEqual(false)
+		// expect(issuefab.Amount.validate([1, "EUR"], undefined)).toEqual(true)
+	})
+	it("allocated", () => {
+		// past
+		expect(Amount.allocated(year, "2023-03-03")).toEqual(0)
+		expect(Amount.allocated(month, "2023-03-03")).toEqual(0)
+		expect(Amount.allocated(week, "2023-03-03")).toEqual(0)
+		expect(Amount.allocated(single, "2023-03-03")).toEqual(0)
+		// same day
+		expect(Amount.allocated(year, "2023-03-04")).toEqual(10)
+		expect(Amount.allocated(month, "2023-03-04")).toEqual(10)
+		expect(Amount.allocated(week, "2023-03-04")).toEqual(10)
+		expect(Amount.allocated(single, "2023-03-04")).toEqual(10)
+		// one day after
+		expect(Amount.allocated(year, "2023-03-05")).toEqual(10)
+		expect(Amount.allocated(month, "2023-03-05")).toEqual(10)
+		expect(Amount.allocated(week, "2023-03-05")).toEqual(10)
+		expect(Amount.allocated(single, "2023-03-05")).toEqual(10)
+		// monday next week
+		expect(Amount.allocated(week, "2023-03-10")).toEqual(20)
+		expect(Amount.allocated(single, "2024-03-10")).toEqual(10)
+		// next week
+		expect(Amount.allocated(year, "2023-03-11")).toEqual(10)
+		expect(Amount.allocated(month, "2023-03-11")).toEqual(10)
+		expect(Amount.allocated(week, "2023-03-11")).toEqual(20)
+		expect(Amount.allocated(single, "2023-03-11")).toEqual(10)
+		// monday next month
+		expect(Amount.allocated(month, "2023-04-03")).toEqual(20)
+		expect(Amount.allocated(week, "2023-04-03")).toEqual(60)
+		expect(Amount.allocated(single, "2024-04-03")).toEqual(10)
+		// next month same day
+		expect(Amount.allocated(year, "2023-04-04")).toEqual(10)
+		expect(Amount.allocated(month, "2023-04-04")).toEqual(20)
+		expect(Amount.allocated(week, "2023-04-04")).toEqual(60)
+		expect(Amount.allocated(single, "2023-04-04")).toEqual(10)
+		// next month next day
+		expect(Amount.allocated(year, "2023-04-05")).toEqual(10)
+		expect(Amount.allocated(month, "2023-04-05")).toEqual(20)
+		expect(Amount.allocated(week, "2023-04-05")).toEqual(60)
+		expect(Amount.allocated(single, "2023-04-05")).toEqual(10)
+		// next year same day
+		expect(Amount.allocated(year, "2024-03-04")).toEqual(20)
+		expect(Amount.allocated(month, "2024-03-04")).toEqual(130)
+		expect(Amount.allocated(week, "2024-03-04")).toEqual(540)
+		expect(Amount.allocated(single, "2024-03-04")).toEqual(10)
+		// exactly next year
+		expect(Amount.allocated(year, "2024-01-01")).toEqual(20)
+		expect(Amount.allocated(month, "2024-01-01")).toEqual(110)
+		expect(Amount.allocated(week, "2024-01-01")).toEqual(450)
+		expect(Amount.allocated(single, "2024-01-01")).toEqual(10)
 	})
 })
--- Amount/index.ts
+++ Amount/index.ts
@@ -1,12 +1,42 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
 
-export type Amount = [number, isoly.Currency]
+export interface Amount {
+	cadence: "year" | "month" | "week" | "single"
+	value: number
+	currency: isoly.Currency
+	created: isoly.Date
+}
 export namespace Amount {
-	export const type = isly.tuple<Amount>(isly.number(), isly.fromIs<isoly.Currency>("Currency", isoly.Currency.is))
+	export const type = isly.object<Amount>({
+		cadence: isly.union(isly.string("year"), isly.string("month"), isly.string("week"), isly.string("single")),
+		value: isly.number(),
+		currency: isly.fromIs("Currency", isoly.Currency.is),
+		created: isly.fromIs("Date", isoly.Date.is),
+	})
 	export const is = type.is
 	export const flaw = type.flaw
-	export function validate(amount: Amount, limit?: Amount): boolean {
-		return amount[0] > 0 && (limit == undefined || (amount[0] <= limit[0] && amount[1] == limit[1]))
+	export function allocated(amount: Amount, date: isoly.Date): number {
+		let result = 0
+		if (amount.created <= date) {
+			if (amount.cadence == "year")
+				for (let d = isoly.Date.firstOfYear(amount.created); d <= date; d = isoly.Date.nextYear(d))
+					result += amount.value
+			else if (amount.cadence == "month")
+				for (let d = isoly.Date.firstOfMonth(amount.created); d <= date; d = isoly.Date.nextMonth(d))
+					result += amount.value
+			else if (amount.cadence == "week") {
+				for (let d = isoly.Date.firstOfWeek(amount.created); d <= date; d = isoly.Date.next(d, 7))
+					result += amount.value
+			} else
+				result = amount.value
+		}
+		return result
 	}
+	// export const type = isly.tuple<Amount>(isly.number(), isly.fromIs<isoly.Currency>("Currency", isoly.Currency.is))
+	// export const is = type.is
+	// export const flaw = type.flaw
+	// export function validate(amount: Amount, limit?: Amount): boolean {
+	// 	return amount[0] > 0 && (limit == undefined || (amount[0] <= limit[0] && amount[1] == limit[1]))
+	// }
 }
