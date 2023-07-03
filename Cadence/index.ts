daf86b53814d1fadc2503b98ef8353f2768f5682
Tobias Liljeblad <tobias@issuefab.com>
2023-07-03 15:20:16 +0200

    fix interval

--- Cadence/index.ts
+++ Cadence/index.ts
@@ -3,12 +3,14 @@ import { isly } from "isly"
 import { Amount } from "../Amount"
 
 export interface Cadence extends Amount {
-	cadence: "year" | "month" | "week" | "single"
+	interval: Cadence.Interval
 	created: isoly.Date
 }
 export namespace Cadence {
+	export const intervals = ["single", "week", "month", "year"] as const
+	export type Interval = typeof intervals[number]
 	export const type = Amount.type.extend<Cadence>({
-		cadence: isly.union(isly.string("year"), isly.string("month"), isly.string("week"), isly.string("single")),
+		interval: isly.union(isly.string("year"), isly.string("month"), isly.string("week"), isly.string("single")),
 		created: isly.fromIs("Date", isoly.Date.is),
 	})
 	export const is = type.is
@@ -16,13 +18,13 @@ export namespace Cadence {
 	export function allocated(cadence: Cadence, date: isoly.Date): number {
 		let result = 0
 		if (cadence.created <= date) {
-			if (cadence.cadence == "year")
+			if (cadence.interval == "year")
 				for (let d = isoly.Date.firstOfYear(cadence.created); d <= date; d = isoly.Date.nextYear(d))
 					result += cadence.value
-			else if (cadence.cadence == "month")
+			else if (cadence.interval == "month")
 				for (let d = isoly.Date.firstOfMonth(cadence.created); d <= date; d = isoly.Date.nextMonth(d))
 					result += cadence.value
-			else if (cadence.cadence == "week") {
+			else if (cadence.interval == "week") {
 				for (let d = isoly.Date.firstOfWeek(cadence.created); d <= date; d = isoly.Date.next(d, 7))
 					result += cadence.value
 			} else
