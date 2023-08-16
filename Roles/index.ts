d6323112968e3e84e97afa0ce25f83a9d49e69b3
Linus Johansson <linus@issuefab.com>
2023-08-16 15:09:28 +0200

    Roles index file

--- Roles/index.ts
+++ Roles/index.ts
@@ -1,32 +1,32 @@
 import { userwidgets } from "@userwidgets/model"
+import { isly } from "isly"
+import { Permissions as RolesPermissions } from "./Permissions"
+
+export type Roles = keyof typeof Roles.record
 
 export namespace Roles {
-	interface Permissions {
-		"*"?: userwidgets.User.Permissions.Application
-		[organizationId: string]: userwidgets.User.Permissions.Organization | undefined
+	export type Permissions = RolesPermissions
+	export const Permissions = RolesPermissions
+	export const type = isly.union<Roles, "admin", "financialController", "user">(
+		isly.string("admin"),
+		isly.string("financialController"),
+		isly.string("user")
+	)
+	export const is = type.is
+	export const flaw = type.flaw
+	export const record = {
+		admin: [...Permissions.flags],
+		financialController: [...Permissions.Issuefab.flags, ...userwidgets.User.Permissions.Organization.flags], //this might also assign app level permission?
+		user: ["user.view", "user.invite", "payment"],
 	}
-	const functions = {
-		user: satisfiesUser,
-		financialController: satisfiesFinancialController,
-	}
-
-	export function satisfies(role: keyof typeof functions, permissions: Permissions, organizationId?: string) {
-		return functions[role](permissions, organizationId)
-	}
-
-	function satisfiesFinancialController(permissions: Permissions, organizationId?: string): boolean {
-		return (
-			(!!permissions[organizationId ?? ""]?.user?.write || !!permissions["*"]?.user?.write) &&
-			(!!permissions[organizationId ?? ""]?.user?.read || !!permissions["*"]?.user?.read) &&
-			(!!permissions[organizationId ?? ""]?.organization?.write || !!permissions["*"]?.organization?.write) &&
-			(!!permissions[organizationId ?? ""]?.organization?.read || !!permissions["*"]?.organization?.read) &&
-			(!!permissions[organizationId ?? ""]?.banking?.write || !!permissions["*"]?.banking?.write) &&
-			(!!permissions[organizationId ?? ""]?.banking?.read || !!permissions["*"]?.banking?.read) &&
-			(!!permissions[organizationId ?? ""]?.delegation?.write || !!permissions["*"]?.delegation?.write) &&
-			(!!permissions[organizationId ?? ""]?.delegation?.read || !!permissions["*"]?.delegation?.read)
-		)
-	}
-	function satisfiesUser(permissions: Permissions, organizationId?: string): boolean {
-		return !!permissions[organizationId ?? ""]?.user?.write || !!permissions["*"]?.user?.write
+	export const roles = Object.keys(record)
+	export function satisfies(role: "admin", permissions: Permissions): boolean
+	export function satisfies(role: Exclude<keyof typeof record, "admin">, permissions: Permissions, id: string): boolean
+	export function satisfies(role: keyof typeof record, permissions: Permissions, id?: string): boolean {
+		return id != undefined
+			? userwidgets.User.Permissions.check(permissions, id, ...record[role])
+			: role == "admin"
+			? userwidgets.User.Permissions.check(permissions, "*", ...record[role])
+			: false
 	}
 }
