ba31e71ced06690233bc6005ee20a154f99b1084
linusarnejohansson <86045901+linusarnejohansson@users.noreply.github.com>
2023-08-18 09:37:39 +0200

    Roles update (#106)
    * Roles.permission
    
    * Roles index file
    
    * Roles tests
    
    * types being types
    
    * remove comments
    
    * updated model
    
    ---------
    
    Co-authored-by: Elias Eriksson <eliaseriksson95@gmail.com>

---
+++ Roles/Permissions.ts
@@ -0,0 +1,97 @@
+import { userwidgets } from "@userwidgets/model"
+import { isly } from "isly"
+
+export type Permissions = userwidgets.User.Permissions<Permissions.Issuefab>
+export namespace Permissions {
+	export type Issuefab = {
+		delegation?: { create?: true; read?: true; edit?: true; view?: true } | true
+		purchase?: { create?: true; read?: true; edit?: true; view?: true } | true
+		costCenter?:
+			| {
+					root?: { create?: true; read?: true; edit?: true; view?: true } | true
+					child?: { create?: true; read?: true; edit?: true; view?: true } | true
+			  }
+			| true
+		reports?: true
+		payment?: { expense?: true; prepaid?: true; card?: true } | true
+		banking?: true
+	}
+	export namespace Issuefab {
+		export const type = isly.object<Permissions>({
+			delegation: isly
+				.object({
+					create: isly.boolean(true).optional(),
+					read: isly.boolean(true).optional(),
+					edit: isly.boolean(true).optional(),
+					view: isly.boolean(true).optional(),
+				})
+				.optional(),
+			purchase: isly
+				.object({
+					create: isly.boolean(true).optional(),
+					read: isly.boolean(true).optional(),
+					edit: isly.boolean(true).optional(),
+					view: isly.boolean(true).optional(),
+				})
+				.optional(),
+			costCenter: isly
+				.object({
+					root: isly
+						.object({
+							create: isly.boolean(true).optional(),
+							read: isly.boolean(true).optional(),
+							edit: isly.boolean(true).optional(),
+							view: isly.boolean(true).optional(),
+						})
+						.optional(),
+					child: isly
+						.object({
+							create: isly.boolean(true).optional(),
+							read: isly.boolean(true).optional(),
+							edit: isly.boolean(true).optional(),
+							view: isly.boolean(true).optional(),
+						})
+						.optional(),
+				})
+				.optional(),
+			reports: isly.boolean(true).optional(),
+			payment: isly
+				.object({
+					expense: isly.boolean(true).optional(),
+					prepaid: isly.boolean(true).optional(),
+					card: isly.boolean(true),
+				})
+				.optional(),
+			banking: isly.boolean(true).optional(),
+		})
+		export const is = type.is
+		export const flaw = type.flaw
+		export const flags = [
+			"delegation.create",
+			"delegation.read",
+			"delegation.edit",
+			"delegation.view",
+			"purchase.create",
+			"purchase.read",
+			"purchase.edit",
+			"purchase.view",
+			"costCenter.root.create",
+			"costCenter.root.read",
+			"costCenter.root.edit",
+			"costCenter.root.view",
+			"costCenter.child.create",
+			"costCenter.child.read",
+			"costCenter.child.edit",
+			"costCenter.child.view",
+			"reports",
+			"payment.expense",
+			"payment.prepaid",
+			"payment.card",
+			"banking",
+		] as const
+	}
+	export const type = userwidgets.User.Permissions.type.create<Permissions>(Issuefab.type)
+	export const is = type.is
+	export const flaw = type.flaw
+	export const flags = Array.from(new Set([...userwidgets.User.Permissions.flags, ...Issuefab.flags]))
+}
--- Roles/index.spec.ts
+++ Roles/index.spec.ts
@@ -3,7 +3,7 @@ import { issuefab } from "../index"
 
 describe("Financial Controller", () => {
 	it("satisfies", () => {
-		const key: userwidgets.User.Key = {
+		const key: userwidgets.User.Key<userwidgets.User.Key.Creatable.Claims, issuefab.Roles.Permissions.Issuefab> = {
 			issuer: "",
 			audience: "",
 			issued: "",
@@ -13,48 +13,76 @@ describe("Financial Controller", () => {
 			email: "",
 			permissions: {
 				organizationId: {
-					organization: { read: true, write: true },
-					user: { read: true, write: true },
-					delegation: { read: true, write: true },
-					banking: { read: true, write: true },
+					org: { view: true, edit: true },
+					user: { invite: true, view: true, admin: true },
+					delegation: { create: true, read: true, edit: true, view: true },
+					costCenter: { root: true, child: { create: true, read: true, edit: true, view: true } },
+					purchase: true,
+					banking: true,
+					payment: true,
+					reports: true,
 				},
 			},
 		}
 		expect(issuefab.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(true)
 		expect(issuefab.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
+		expect(issuefab.Roles.satisfies("admin", key.permissions)).toEqual(false)
 		key.permissions = {
 			["*"]: {
-				organization: { read: true },
+				org: { edit: true },
 			},
 			organizationId: {
-				organization: { write: true },
-				user: { read: true, write: true },
-				delegation: { read: true, write: true },
-				banking: { read: true, write: true },
+				org: { view: true },
+				user: { invite: true, view: true, admin: true },
+				delegation: { create: true, read: true, edit: true, view: true },
+				costCenter: { root: true, child: { create: true, read: true, edit: true, view: true } },
+				purchase: true,
+				banking: true,
+				payment: true,
+				reports: true,
 			},
 		}
 		expect(issuefab.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(true)
-		expect(issuefab.Roles.satisfies("financialController", key.permissions)).toEqual(false)
+		expect(issuefab.Roles.satisfies("financialController", key.permissions, "")).toEqual(false)
 		expect(issuefab.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
-		expect(issuefab.Roles.satisfies("user", key.permissions)).toEqual(false)
+		expect(issuefab.Roles.satisfies("user", key.permissions, "")).toEqual(false)
+		expect(issuefab.Roles.satisfies("admin", key.permissions)).toEqual(false)
 		key.permissions = {
 			organizationId: {
-				organization: { read: false, write: false },
-				user: { read: false, write: true },
-				delegation: { read: false, write: false },
-				banking: { read: false, write: false },
+				user: { invite: true, view: true },
+				payment: true,
 			},
 		}
 		expect(issuefab.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(false)
-		expect(issuefab.Roles.satisfies("financialController", key.permissions)).toEqual(false)
+		expect(issuefab.Roles.satisfies("financialController", key.permissions, "")).toEqual(false)
 		expect(issuefab.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
-		expect(issuefab.Roles.satisfies("user", key.permissions)).toEqual(false)
+		expect(issuefab.Roles.satisfies("user", key.permissions, "")).toEqual(false)
 		key.permissions = {
-			["*"]: { user: { write: true } },
+			["*"]: {
+				user: { invite: true, view: true },
+				payment: true,
+			},
 		}
 		expect(issuefab.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(false)
-		expect(issuefab.Roles.satisfies("financialController", key.permissions)).toEqual(false)
+		expect(issuefab.Roles.satisfies("financialController", key.permissions, "")).toEqual(false)
 		expect(issuefab.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
-		expect(issuefab.Roles.satisfies("user", key.permissions)).toEqual(true)
+		expect(issuefab.Roles.satisfies("user", key.permissions, "")).toEqual(true)
+		key.permissions = {
+			"*": {
+				org: { view: true, edit: true, create: true },
+				user: { invite: true, view: true, admin: true },
+				delegation: { create: true, read: true, edit: true, view: true },
+				costCenter: { root: true, child: { create: true, read: true, edit: true, view: true } },
+				purchase: true,
+				banking: true,
+				payment: true,
+				reports: true,
+				app: true,
+			},
+			whatever: { org: {} },
+		}
+		expect(issuefab.Roles.satisfies("admin", key.permissions)).toEqual(true)
+		expect(issuefab.Roles.satisfies("financialController", key.permissions, "organizationId")).toEqual(true)
+		expect(issuefab.Roles.satisfies("user", key.permissions, "organizationId")).toEqual(true)
 	})
 })
