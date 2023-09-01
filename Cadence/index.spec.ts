9cf1f6a2a9f10e531be41c745c2bb775f022ca17
Elias Eriksson <elias@issuefab.com>
2023-09-01 09:30:23 +0200

    Sustainable days (#110)
    * unsustainable things
    
    * iterative sustainable
    
    * using approximation to speed it up
    
    * only using relevant days
    
    * woupsies
    
    * two tests
    
    * challenging test
    
    * -1 = unsustainable
    
    * more tests
    
    * commented some ideas of improvements
    
    * updated fault test expectation
    
    * fixes
    
    * fixes
    
    * added day to isly type

--- Cadence/index.spec.ts
+++ Cadence/index.spec.ts
@@ -1,4 +1,6 @@
+import { isoly } from "isoly"
 import { issuefab } from "../index"
+import { Cadence } from "."
 
 describe("Amount", () => {
 	const year: issuefab.Cadence = {
@@ -15,6 +17,12 @@ describe("Amount", () => {
 	}
 	const week: issuefab.Cadence = {
 		interval: "week",
+		value: 10,
+		currency: "EUR",
+		created: "2023-03-04",
+	}
+	const day: issuefab.Cadence = {
+		interval: "day",
 		value: 10,
 		currency: "EUR",
 		created: "2023-03-04",
@@ -84,5 +92,223 @@ describe("Amount", () => {
 		expect(issuefab.Cadence.allocated(month, "2024-01-01")).toEqual(110)
 		expect(issuefab.Cadence.allocated(week, "2024-01-01")).toEqual(450)
 		expect(issuefab.Cadence.allocated(single, "2024-01-01")).toEqual(10)
+		// days
+		expect(issuefab.Cadence.allocated(day, "2023-03-04")).toEqual(10)
+		expect(issuefab.Cadence.allocated(day, "2023-03-05")).toEqual(20)
+		expect(issuefab.Cadence.allocated(day, "2023-03-06")).toEqual(30)
+		expect(issuefab.Cadence.allocated(day, "2023-03-06", { cap: 20 })).toEqual(20)
+	})
+	it("sustainable", () => {
+		let parent: issuefab.Cadence
+		let children: issuefab.Cadence[]
+		let end: isoly.Date
+		let date: isoly.Date
+		let used: number
+
+		// all used
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "single",
+			value: 200,
+		}
+		children = [
+			{
+				created: "2023-01-01",
+				currency: "EUR",
+				interval: "day",
+				value: 1,
+			},
+		]
+		expect(issuefab.Cadence.sustainable(parent, children, "2024-01-01")).toEqual(199)
+
+		// calculating the end date
+		end = "2023-02-01"
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "single",
+			value: 200,
+		}
+		children = [
+			{
+				created: "2023-01-01",
+				currency: "EUR",
+				interval: "day",
+				value: 1,
+			},
+		]
+		expect(isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))).toEqual(end)
+
+		// slightly before the end
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "single",
+			value: 350,
+		}
+		children = [
+			{
+				created: "2023-01-30",
+				currency: "EUR",
+				interval: "year",
+				value: 10,
+			},
+			{
+				created: "2023-02-20",
+				currency: "EUR",
+				interval: "month",
+				value: 5,
+			},
+			{
+				created: "2023-05-05",
+				currency: "EUR",
+				interval: "week",
+				value: 3,
+			},
+		]
+		end = "2025-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		used = children.reduce((result, child) => result + Cadence.allocated(child, date), 0)
+		expect(date <= end).toEqual(true)
+		expect(parent.created <= date).toEqual(true)
+		expect(used <= issuefab.Cadence.allocated(parent, end)).toEqual(true)
+
+		// good approximation
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "single",
+			value: 140,
+		}
+		children = [
+			{
+				created: "2023-01-05",
+				currency: "EUR",
+				interval: "day",
+				value: 10,
+			},
+			{
+				created: "2023-01-13",
+				currency: "EUR",
+				interval: "day",
+				value: 5,
+			},
+		]
+		end = "2024-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		used = children.reduce((result, child) => result + Cadence.allocated(child, date), 0)
+		expect(date <= end).toEqual(true)
+		expect(parent.created <= date).toEqual(true)
+		expect(used == 140).toEqual(true)
+
+		// sustainable only day 0
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "single",
+			value: 140,
+		}
+		children = [
+			{
+				created: "2023-01-01",
+				currency: "EUR",
+				interval: "day",
+				value: 140,
+			},
+		]
+		end = "2024-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		expect(date).toEqual(parent.created)
+
+		// unsustainable from day 0
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "single",
+			value: 140,
+		}
+		children = [
+			{
+				created: "2023-01-01",
+				currency: "EUR",
+				interval: "week",
+				value: 200,
+			},
+		]
+		end = "2024-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		expect(date).toEqual("2022-12-31")
+
+		// parent growing faster than children
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "week",
+			value: 140,
+		}
+		children = [
+			{
+				created: "2023-01-01",
+				currency: "EUR",
+				interval: "week",
+				value: 100,
+			},
+		]
+		end = "2024-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		expect(date).toEqual(end)
+
+		// parent growing slower than children
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "month",
+			value: 400,
+		}
+		children = [
+			{
+				created: "2023-02-01",
+				currency: "EUR",
+				interval: "month",
+				value: 400,
+			},
+			{
+				created: "2023-03-01",
+				currency: "EUR",
+				interval: "month",
+				value: 100,
+			},
+		]
+		end = "2024-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		expect(date).toEqual("2023-06-30")
+
+		// yearly growth over long time
+		// slowest test so far ~8-10ms
+		// this calculation is unrealistic to exist in the application
+		parent = {
+			created: "2023-01-01",
+			currency: "EUR",
+			interval: "year",
+			value: 400,
+		}
+		children = [
+			{
+				created: "2025-02-01",
+				currency: "EUR",
+				interval: "month",
+				value: 200,
+			},
+			{
+				created: "2023-03-01",
+				currency: "EUR",
+				interval: "year",
+				value: 100,
+			},
+		]
+		end = "2034-01-01"
+		date = isoly.Date.next(parent.created, issuefab.Cadence.sustainable(parent, children, end))
+		expect(date < end).toEqual(true)
 	})
 })
--- Cadence/index.ts
+++ Cadence/index.ts
@@ -7,32 +7,128 @@ export interface Cadence extends Amount {
 	created: isoly.Date
 }
 export namespace Cadence {
-	export const intervals = ["single", "week", "month", "year"] as const
+	export const intervals = ["single", "day", "week", "month", "year"] as const
 	export type Interval = typeof intervals[number]
 	export const type = Amount.type.extend<Cadence>({
-		interval: isly.union(isly.string("year"), isly.string("month"), isly.string("week"), isly.string("single")),
+		interval: isly.union<Interval, "single", "day", "week", "month", "year">(
+			isly.string("single"),
+			isly.string("day"),
+			isly.string("week"),
+			isly.string("month"),
+			isly.string("year")
+		),
 		created: isly.fromIs("Date", isoly.Date.is),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
-	export function allocated(cadence: Cadence, date: isoly.Date): number {
+	export function allocated(cadence: Cadence, date: isoly.Date, options?: { cap?: number }): number {
 		let result = 0
 		if (cadence.created <= date) {
-			if (cadence.interval == "year")
-				for (let d = isoly.Date.firstOfYear(cadence.created); d <= date; d = isoly.Date.nextYear(d))
-					result += cadence.value
-			else if (cadence.interval == "month")
-				for (let d = isoly.Date.firstOfMonth(cadence.created); d <= date; d = isoly.Date.nextMonth(d))
-					result += cadence.value
-			else if (cadence.interval == "week") {
-				for (let d = isoly.Date.firstOfWeek(cadence.created); d <= date; d = isoly.Date.next(d, 7))
-					result += cadence.value
-			} else
+			if (cadence.interval == "year") {
+				const initial = isoly.Date.firstOfYear(cadence.created)
+				result = Math.max(0, (isoly.Date.getYear(date) - isoly.Date.getYear(initial)) * cadence.value + cadence.value)
+			} else if (cadence.interval == "month") {
+				const initial = isoly.Date.firstOfMonth(cadence.created)
+				result = Math.max(
+					0,
+					((isoly.Date.getYear(date) - isoly.Date.getYear(initial)) * 12 +
+						(isoly.Date.getMonth(date) - isoly.Date.getMonth(initial))) *
+						cadence.value +
+						cadence.value
+				)
+			} else if (cadence.interval == "week") {
+				const initial = isoly.Date.firstOfWeek(cadence.created)
+				result = Math.max(0, Math.trunc(duration(date, initial) / 7) * cadence.value + cadence.value)
+			} else if (cadence.interval == "day")
+				result = Math.max(0, duration(date, cadence.created) * cadence.value + cadence.value)
+			else
 				result = cadence.value
 		}
-		return result
+		return options?.cap == undefined ? result : Math.min(options.cap, result)
 	}
+	function partition<T>(array: T[], filter: (item: T) => boolean): [T[], T[]] {
+		const pass: T[] = []
+		const fail: T[] = []
+		array.forEach(item => (filter(item) ? pass.push(item) : fail.push(item)))
+		return [pass, fail]
+	}
+	/**
+	 * formula described here https://github.com/issuefab/app/issues/232
+	 */
+	function approximate(self: Cadence, children: Cadence[], date: isoly.Date, options?: { cap?: number }): number {
+		const [cadences, singles] = partition(children, child => child.interval != "single")
+		const funds =
+			Math.max(allocated(self, date), options?.cap ?? 0) -
+			singles.reduce((result, cadence) => result + cadence.value, 0)
+		const rates = cadences.map(cadence => {
+			const days = Math.abs(duration(date, cadence.created)) + 1
+			const allocated = Cadence.allocated(cadence, date)
+			return isoly.Currency.divide(cadence.currency, allocated, days)
+		})
+		const numerator =
+			funds +
+			cadences.reduce((result, cadence, index) => {
+				const time = Math.abs(duration(cadence.created, self.created))
+				const rate = rates[index]
+				return result + rate * time
+			}, 0)
+		const denominator = rates.reduce((result, cost) => result + cost, 0)
+		const result = numerator / denominator
+		return Math.trunc(result)
+	}
+	/**
+	 * Potential optimizations:
+	 * 1: dynamically change the next function
+	 * and use it for iteration
+	 * 2: improve the approximation by changing "y"
+	 * in the formula to be any line instead of constant
+	 * 3: steps in bigger increments and go backwards if overshooting
+	 * 4: frontend can put the work in a background worker
+	 */
+	export function sustainable(
+		self: Cadence,
+		children: Cadence[],
+		date: isoly.Date,
+		options?: { cap?: number }
+	): number {
+		const [cadences, singles] = partition(children, child => child.interval != "single")
+		const cap =
+			Math.max(allocated(self, date), options?.cap ?? 0) -
+			singles.reduce((result, cadence) => result + cadence.value, 0)
 
+		const max = duration(date, self.created)
+		const approximation = Math.max(0, Math.min(max, approximate(self, children, date, { cap })))
+		const approximationDate = isoly.Date.next(self.created, approximation)
+		const childCost = children.reduce((result, cadence) => result + allocated(cadence, approximationDate), 0)
+		const approximateCap =
+			Math.min(allocated(self, approximationDate), cap) - singles.reduce((result, cadence) => result + cadence.value, 0)
+
+		let days: number
+		if (childCost <= approximateCap)
+			for (days = approximation; days < max; days++) {
+				const next = isoly.Date.next(self.created, days)
+				const cap =
+					Math.min(allocated(self, next), approximateCap) -
+					singles.reduce((result, cadence) => result + cadence.value, 0)
+				const sum = cadences.reduce((r, c) => r + allocated(c, next), 0)
+				if (sum >= cap)
+					break
+			}
+		else
+			for (days = approximation; days > -1; days--) {
+				const next = isoly.Date.next(self.created, days)
+				const cap =
+					Math.min(allocated(self, next), approximateCap) -
+					singles.reduce((result, cadence) => result + cadence.value, 0)
+				const sum = cadences.reduce((r, c) => r + allocated(c, next), 0)
+				if (sum <= cap)
+					break
+			}
+		return days
+	}
+	function duration(first: isoly.Date | isoly.DateTime, other: isoly.Date | isoly.DateTime): number {
+		return Math.trunc((new Date(first).getTime() - new Date(other).getTime()) / 1_000 / 3_600 / 24)
+	}
 	export function getDate(cadence: Cadence) {
 		let result: isoly.Date
 		switch (cadence.interval) {
