f28e1d4199cf831508a962ebc3342de4f8b6281e
Elias Eriksson <eliaseriksson95@gmail.com>
2023-09-18 10:59:05 +0200

    updated packages

--- Key/Creatable.ts
+++ Key/Creatable.ts
@@ -1,18 +1,10 @@
 import { userwidgets } from "@userwidgets/model"
 import { isly } from "isly"
-import { Roles } from "../Roles"
 
-export type Creatable = userwidgets.User.Key.Creatable<
-	userwidgets.User.Key.Creatable.Claims,
-	Roles.Permissions.Issuefab
->
+export type Creatable = userwidgets.User.Key.Creatable<userwidgets.User.Key.Creatable.Claims>
 export namespace Creatable {
-	export const type: isly.Type<Creatable> = userwidgets.User.Key.Creatable.type.create<
-		userwidgets.User.Key.Creatable.Claims,
-		Roles.Permissions
-	>({
-		permissions: Roles.Permissions.type,
-	})
+	export const type: isly.Type<Creatable> =
+		userwidgets.User.Key.Creatable.type.create<userwidgets.User.Key.Creatable.Claims>()
 	export const is = type.is
 	export const flaw = type.flaw
 }
