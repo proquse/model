5a2d43a10b2e1a5f4c5a387db72370e7e677f75c
Linus Johansson <linus@issuefab.com>
2024-02-19 14:21:34 +0100

    Delegation rewritten to use usage

--- Delegation/index.ts
+++ Delegation/index.ts
@@ -14,8 +14,8 @@ export interface Delegation extends Delegation.Creatable {
 	id: Delegation.Identifier
 	created: isoly.DateTime
 	modified: isoly.DateTime
-	purchases: Purchase[]
-	delegations: Delegation[]
+	usage: (Purchase | Delegation)[]
+	type: "delegation"
 }
 export namespace Delegation {
 	export type Identifier = DelegationIdentifier
@@ -26,8 +26,13 @@ export namespace Delegation {
 		id: Identifier.type,
 		created: isly.fromIs<isoly.DateTime>("DateTime", isoly.DateTime.is),
 		modified: isly.fromIs<isoly.DateTime>("DateTime", isoly.DateTime.is),
-		purchases: isly.array(isly.fromIs("Purchase", Purchase.is)),
-		delegations: isly.array(isly.lazy(() => type, "Delegation")),
+		usage: isly.array(
+			isly.union(
+				Purchase.type,
+				isly.lazy(() => type, "delegation")
+			)
+		),
+		type: isly.string("delegation"),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
@@ -39,8 +44,8 @@ export namespace Delegation {
 			id: override?.id ?? cryptly.Identifier.generate(Identifier.length),
 			created: override?.created ?? now,
 			modified: override?.modified ?? now,
-			purchases: override?.purchases ?? [],
-			delegations: override?.delegations ?? [],
+			usage: override?.usage ?? [],
+			type: "delegation",
 		}
 	}
 	export const change = changeDelegation
@@ -50,25 +55,28 @@ export namespace Delegation {
 	): { root: T; removed: Delegation } | undefined {
 		let result: { root: T; removed: Delegation } | undefined
 		const index = roots.findIndex(
-			root => root.id == id && "purchases" in root && (result = { root: root, removed: root })
+			root => root.id == id && root.type == "delegation" && (result = { root: root, removed: root })
 		)
 		if (index >= 0)
 			roots.splice(index, 1)
 		return result
 			? result
 			: roots.find(
-					root => (result = (result => (!result ? result : { ...result, root }))(remove(root.delegations, id)))
+					root =>
+						(result = (result => (!result ? result : { ...result, root }))(
+							remove(root.usage.filter(Delegation.is || CostCenter.is), id)
+						))
 			  ) && result
 	}
 	export const find = Object.assign(findDelegation, { node: findNode })
 	export function findUser<T extends Delegation | CostCenter>(roots: T[], email: userwidgets.Email): Delegation[] {
 		const result: Delegation[] = []
 		for (const root of roots) {
-			if ("costCenters" in root)
-				result.push(...findUser(root.costCenters, email))
+			if (root.type == "costCenter")
+				result.push(...findUser(root.usage.filter(CostCenter.is), email))
 			else
 				root.to.includes(email) && result.push(root)
-			result.push(...findUser(root.delegations, email))
+			result.push(...findUser(root.usage.filter(Delegation.is), email))
 		}
 		return result
 	}
@@ -79,7 +87,7 @@ export namespace Delegation {
 		let result: { root: TResult; found: TResult } | undefined
 		return roots.find(
 			root =>
-				root.delegations.find(delegation => delegation.id == id) &&
+				root.usage.filter(Delegation.is).find(delegation => delegation.id == id) && //is this typesafe?
 				(result = { root: root as any as TResult, found: root as any as TResult })
 		)
 			? result
@@ -87,7 +95,7 @@ export namespace Delegation {
 					root =>
 						(result = (result =>
 							!result ? result : ({ ...result, root } as unknown as { root: TResult; found: TResult }))(
-							findParent(root.delegations, id)
+							findParent(root.usage.filter(Delegation.is), id) //is this typesafe?
 						))
 			  ) && result
 	}
@@ -99,7 +107,9 @@ export namespace Delegation {
 		return result
 			? result
 			: roots.find(
-					root => (result = findParents(root.delegations, id)) && (result = [root as unknown as TResult, ...result])
+					root =>
+						(result = findParents(root.usage.filter(Delegation.is), id)) && //is this typesafe?
+						(result = [root as unknown as TResult, ...result])
 			  ) && result
 	}
 	export function path<
@@ -112,7 +122,7 @@ export namespace Delegation {
 			result = [...path, found]
 		else if (nodes.length)
 			for (const node of nodes) {
-				const nodes = "costCenters" in node ? [...node.costCenters, ...node.delegations] : node.delegations
+				const nodes = node.usage
 				result = Delegation.path(nodes as T[], id, [...path, node] as TResult[])
 				if (result)
 					break
@@ -129,9 +139,12 @@ export namespace Delegation {
 		if (node) {
 			const date = options?.date ?? isoly.Date.now()
 			const allocated = Cadence.allocated(node.amount, date, { limit: options?.limit })
-			const children = ("costCenters" in node ? [...node.delegations, ...node.costCenters] : node.delegations).map(
-				node => node.amount
+			const children = node.usage.reduce<Cadence[]>(
+				(result, node) => result.concat(node.type != "purchase" ? node.amount : []),
+				[]
 			)
+			// .filter((value): value is CostCenter | Delegation => value.type != "purchase")
+			// .map(node => node.amount)
 			const sustainable = isoly.Date.next(
 				node.amount.created,
 				Cadence.sustainable(node.amount, children, date, { limit: allocated })
@@ -148,23 +161,45 @@ export namespace Delegation {
 		options?: { date?: isoly.Date }
 	): T[] {
 		descendants
+			//does this work properly? it seems fishy
 			.map(descendant => path(ancestors, descendant.id)?.slice(1).concat(descendant))
-			.filter((value: (CostCenter | Delegation)[] | undefined): value is (CostCenter | Delegation)[] => !!value)
-			.forEach(path => sustainablePath(path, options))
+			// .filter((value: (CostCenter | Delegation)[] | undefined): value is (CostCenter | Delegation)[] => !!value)
+			.forEach(path => !!path && sustainablePath(path, options))
 		return descendants
 	}
 	export const spent = Object.assign(calculateSpent, { balance: calculateSpentBalance })
 	function calculateSpent(root: Delegation | CostCenter, options?: { vat?: boolean }): number {
-		return [...root.delegations, ...("costCenters" in root ? root.costCenters : [])].reduce(
-			(result, current) => result + spent(current, { ...options }),
-			!("purchases" in root)
-				? 0
-				: root.purchases.reduce(
-						(result, purchase) =>
-							isoly.Currency.add(root.amount.currency, result, Purchase.spent(purchase, { vat: options?.vat })),
-						0
-				  )
+		return root.usage.reduce(
+			(result, current) =>
+				Purchase.is(current)
+					? isoly.Currency.add(root.amount.currency, result, Purchase.spent(current, { vat: options?.vat }))
+					: result + spent(current, { ...options }),
+			0
 		)
+
+		// root.usage
+		// 	.filter((value): value is Delegation | CostCenter => Delegation.is(value) || CostCenter.is(value))
+		// 	.reduce(
+		// 		(result, current) => result + spent(current, { ...options }),
+		// 		root.usage
+		// 			.filter(Purchase.is)
+		// 			.reduce(
+		// 				(result, purchase) =>
+		// 					isoly.Currency.add(root.amount.currency, result, Purchase.spent(purchase, { vat: options?.vat })),
+		// 				0
+		// 			)
+		// 	)
+
+		// return [...root.delegations, ...("costCenters" in root ? root.costCenters : [])].reduce(
+		// 	(result, current) => result + spent(current, { ...options }),
+		// 	!("purchases" in root)
+		// 		? 0
+		// 		: root.purchases.reduce(
+		// 				(result, purchase) =>
+		// 					isoly.Currency.add(root.amount.currency, result, Purchase.spent(purchase, { vat: options?.vat })),
+		// 				0
+		// 		  )
+		// )
 	}
 	function calculateSpentBalance(root: Delegation | CostCenter, date: isoly.Date, options?: { vat?: boolean }): number
 	function calculateSpentBalance(root: Delegation | CostCenter, allocated: number, options?: { vat?: boolean }): number
@@ -178,24 +213,33 @@ export namespace Delegation {
 	}
 	export const allocated = Object.assign(calculateAllocated, { balance: calculateAllocatedBalance })
 	function calculateAllocated(root: Delegation | CostCenter, date: isoly.Date): number {
-		return (
-			("purchases" in root
-				? root.purchases.reduce(
-						(result, purchase) =>
-							isoly.Currency.add(root.amount.currency, result, Cadence.allocated(purchase.payment.limit, date)),
-						0
-				  )
-				: root.costCenters.reduce(
-						(result, costCenter) =>
-							isoly.Currency.add(root.amount.currency, result, Cadence.allocated(costCenter.amount, date)),
-						0
-				  )) +
-			root.delegations.reduce(
-				(result, delegation) =>
-					isoly.Currency.add(root.amount.currency, result, Cadence.allocated(delegation.amount, date)),
-				0
-			)
+		return root.usage.reduce(
+			(result, current) =>
+				CostCenter.is(current)
+					? isoly.Currency.add(root.amount.currency, result, Cadence.allocated(current.amount, date))
+					: Delegation.is(current)
+					? isoly.Currency.add(root.amount.currency, result, Cadence.allocated(current.amount, date))
+					: Purchase.is(current)
+					? isoly.Currency.add(root.amount.currency, result, Cadence.allocated(current.payment.limit, date))
+					: 0,
+			0
 		)
+		// ("purchases" in root
+		// 	? root.purchases.reduce(
+		// 			(result, purchase) =>
+		// 				isoly.Currency.add(root.amount.currency, result, Cadence.allocated(purchase.payment.limit, date)),
+		// 			0
+		// 	  )
+		// 	: root.costCenters.reduce(
+		// 			(result, costCenter) =>
+		// 				isoly.Currency.add(root.amount.currency, result, Cadence.allocated(costCenter.amount, date)),
+		// 			0
+		// 	  )) +
+		// root.delegations.reduce(
+		// 	(result, delegation) =>
+		// 		isoly.Currency.add(root.amount.currency, result, Cadence.allocated(delegation.amount, date)),
+		// 	0
+		// )
 	}
 	function calculateAllocatedBalance(root: Delegation | CostCenter, date: isoly.Date): number {
 		return isoly.Currency.subtract(root.amount.currency, Cadence.allocated(root.amount, date), allocated(root, date))
@@ -210,27 +254,48 @@ export namespace Delegation {
 			delegation.amount.created,
 			Cadence.sustainable(
 				delegation.amount,
-				delegation.delegations.map(d => d.amount).concat(delegation.purchases.map(p => p.payment.limit)),
+				delegation.usage.map(value => (value.type == "delegation" ? value.amount : value.payment.limit)),
+				// delegation.delegations.map(d => d.amount).concat(delegation.purchases.map(p => p.payment.limit)),
 				date,
 				{ limit: allocated }
 			)
 		)
+		// const sustainable = isoly.Date.next(
+		// 	delegation.amount.created,
+		// 	Cadence.sustainable(
+		// 		delegation.amount,
+		// 		delegation.delegations.map(d => d.amount).concat(delegation.purchases.map(p => p.payment.limit)),
+		// 		date,
+		// 		{ limit: allocated }
+		// 	)
+		// )
 		return (
 			allocated > 0 &&
 			(!options?.limit || allocated <= options.limit) &&
 			isoly.DateTime.getDate(delegation.created) <= delegation.amount.created &&
 			delegation.amount.created <= sustainable &&
 			(!options?.currency || delegation.amount.currency == options.currency) &&
-			delegation.delegations.every(
-				d =>
-					delegation.created <= d.created &&
-					Delegation.validate(d, { date: sustainable, currency: delegation.amount.currency, spent: options?.spent })
-			) &&
-			delegation.purchases.every(
-				p =>
-					delegation.created <= p.created &&
-					Purchase.validate(p, { date: sustainable, currency: delegation.amount.currency, spent: options?.spent })
+			delegation.usage.every(value =>
+				value.type == "delegation"
+					? delegation.created <= value.created &&
+					  Delegation.validate(value, {
+							date: sustainable,
+							currency: delegation.amount.currency,
+							spent: options?.spent,
+					  })
+					: delegation.created <= value.created &&
+					  Purchase.validate(value, { date: sustainable, currency: delegation.amount.currency, spent: options?.spent })
 			)
+			// delegation.delegations.every(
+			// 	d =>
+			// 		delegation.created <= d.created &&
+			// 		Delegation.validate(d, { date: sustainable, currency: delegation.amount.currency, spent: options?.spent })
+			// ) &&
+			// delegation.purchases.every(
+			// 	p =>
+			// 		delegation.created <= p.created &&
+			// 		Purchase.validate(p, { date: sustainable, currency: delegation.amount.currency, spent: options?.spent })
+			// )
 		)
 	}
 }
