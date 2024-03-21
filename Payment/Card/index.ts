5df1fd71150771976dc0ce68c57c2b5fdc8a74d4
Elias Eriksson <eliaseriksson95@gmail.com>
2024-03-21 14:58:40 +0100

    type fix

--- Payment/Card/index.ts
+++ Payment/Card/index.ts
@@ -1,13 +1,13 @@
 import { isly } from "isly"
 import { Creatable as CardCreatable } from "./Creatable"
 
-export interface Card extends Card.Creatable {
+export interface Card extends Omit<Card.Creatable, "key"> {
 	reference: string
 	token?: string
 }
 export namespace Card {
 	export import Creatable = CardCreatable
-	export const type = Creatable.type.extend<Card>({
+	export const type = Creatable.type.omit(["key"]).extend<Card>({
 		reference: isly.string(),
 		token: isly.string().optional(),
 	})
