a43dbaba6fa6a9e2cae580d3237c37eaa2a8b7ae
Elias Eriksson <eliaseriksson95@gmail.com>
2024-04-08 11:47:59 +0200

    removed comments

--- Transaction/Operation/Type.ts
+++ Transaction/Operation/Type.ts
@@ -1,9 +1,5 @@
 import { isly } from "isly"
 
-// for poms:
-// authorize if: type=authorization
-// capture if: type=settlement
-// refund if: type=reversal
 export type Type = typeof Type.values[number]
 export namespace Type {
 	export const values = ["authorize", "capture", "refund"] as const
